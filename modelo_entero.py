b643403a5f4f4d05546ab2f261c60b0e86327514
MrCamilo5 <59849958+MrCamilo5@users.noreply.github.com>
2020-02-17 10:50:35 -0500

    Add files via upload

---
+++ modelo_entero.py
@@ -0,0 +1,20 @@
+from docplex.mp.model import Model
+
+vino=[(i,j) for i in range(3) for j in range(3)]
+
+modelo=Model(name='Programacion_Entera')
+
+x=modelo.integer_var_dict(vino)
+
+for i in range(3):
+    modelo.add_constraint(modelo.sum(x[j,i] for j in range(3))== 7)
+
+for j in range(3):
+    modelo.add_constraint(modelo.sum(x[i,j] for i in range(3) )== 7)
+
+for j in range(3):
+    modelo.add_constraint(modelo.sum((x[i,j]/(i+1)) for i in range(2) )==3.5)
+
+modelo.maximize(1)
+resultado=modelo.solve()
+resultado.display()
